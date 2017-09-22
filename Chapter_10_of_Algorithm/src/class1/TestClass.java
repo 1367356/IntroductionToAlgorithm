f94991793c32ca5ab7c892a0eb573b64f616d540
1367356 <1185978642@qq.com>
2017-09-22 18:07:23 +0800

    10-13Chapter

--- Chapter_10_of_Algorithm/src/class1/TestClass.java
+++ Chapter_10_of_Algorithm/src/class1/TestClass.java
@@ -4,16 +4,30 @@ import java.io.FileNotFoundException;
 
 import org.omg.PortableInterceptor.INACTIVE;
 
+import class1.DoubleDirectionLinkedList.doubleNode;
 import class1.LinkedList.Node;
 
 public class TestClass {
 
 				public static void main(String[] args) throws FileNotFoundException {
-                  LinkedList<Integer> ll=new LinkedList<Integer>();
-                  ll.insert(6);
-                  ll.insert(6);
-                  Node first=ll.first;
-                  ll.print(first);
+					//单链表测试
+//                  LinkedList<Integer> ll=new LinkedList<Integer>();
+//                  ll.insert(6);
+//                  ll.insert(6);
+//                  Node first=ll.first;
+//                  ll.print(first);
+					
+					DoubleDirectionLinkedList<Integer> ddll=new DoubleDirectionLinkedList<>();
+					ddll.insert(54);
+					ddll.insert(5);
+					ddll.insert(40);
+					ddll.insert(45);
+					ddll.insert(98);
+					doubleNode node=ddll.head;   //逆向打印
+					ddll.print(node);
+					System.out.println("删除后");
+					ddll.delete(node, 40);
+					ddll.print(node);
 
 
 }
