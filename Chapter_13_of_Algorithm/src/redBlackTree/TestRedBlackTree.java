31f9f5c12d82c1d7b5739aa90dbec997a5aa0eb8
1367356 <1185978642@qq.com>
2017-09-27 19:22:45 +0800

    commit

---
+++ Chapter_13_of_Algorithm/src/redBlackTree/TestRedBlackTree.java
@@ -0,0 +1,11 @@
+package redBlackTree;
+
+public class TestRedBlackTree {
+		public static void main(String[] args) {
+			RedBlackTree<Integer> rbt=new RedBlackTree<Integer>();
+			rbt.insert(5);
+			rbt.insert(9);
+			rbt.insert(2);
+			rbt.print();
+		}
+}
