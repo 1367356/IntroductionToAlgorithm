79126c586f1a39797311e02cf3bf7d1efddaaa75
1367356 <1185978642@qq.com>
2017-11-10 11:01:05 +0800

    commit
    commit

---
+++ Chapter_04_of_Algorithm/src/mydatastructure/Pojo.java
@@ -0,0 +1,26 @@
+package mydatastructure;
+
+public class Pojo {
+
+	int data1;
+	int data2;
+	int data3;
+	public int getData1() {
+		return data1;
+	}
+	public void setData1(int data1) {
+		this.data1 = data1;
+	}
+	public int getData2() {
+		return data2;
+	}
+	public void setData2(int data2) {
+		this.data2 = data2;
+	}
+	public int getData3() {
+		return data3;
+	}
+	public void setData3(int data3) {
+		this.data3 = data3;
+	}
+}
